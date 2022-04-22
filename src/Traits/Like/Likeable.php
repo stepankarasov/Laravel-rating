52257b2cd5db280aa8e34b6e1174653ca11a652f
Guillermo Rodriguez <guillermo.blackspot@gmail.com>
2022-04-22 14:38:11 -0500

    Adding scopes for eager loading (likes and dislikes count)

--- src/Traits/Like/Likeable.php
+++ src/Traits/Like/Likeable.php
@@ -2,13 +2,14 @@
 
 namespace Nagy\LaravelRating\Traits\Like;
 
+use Nagy\LaravelRating\LaravelRating;
 use Nagy\LaravelRating\Models\Rating;
 
 trait Likeable
 {
     public function likes()
     {
-        return $this->morphMany(Rating::class, 'rateable');
+        return $this->morphMany(Rating::class, 'rateable')->typeLike();
     }
 
     public function likesDislikesCount()
@@ -24,5 +25,26 @@ trait Likeable
     public function dislikesCount()
     {
         return $this->likes()->where('value', 0)->count();
+    }
+
+
+    /**
+     * Eager loading
+     */
+
+    public function scopeWithLikesCount($query)
+    {
+        return $query->withCount(['likes as likes_count' => function($query){
+            $query->where('type', LaravelRating::TYPE_LIKE)
+                ->where('value',1);
+        }]);       
+    }
+
+    public function scopeWithDislikesCount($query)
+    {
+        return $query->withCount(['likes as dislikes_count' => function($query){
+            $query->where('type', LaravelRating::TYPE_LIKE)
+                ->where('value',0);
+        }]);
     }
 }
